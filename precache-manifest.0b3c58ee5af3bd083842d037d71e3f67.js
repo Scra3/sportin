09e3544df3eede81ce3be574b9d9eab31e93823d
alban bertolini <albanbertolini@pc-17.home>
2021-11-19 14:48:26 +0100

    Updates

---
+++ precache-manifest.0b3c58ee5af3bd083842d037d71e3f67.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510540866a4695acf8371e748e7795ec",
+    "url": "/sportin/index.html"
+  },
+  {
+    "revision": "5f25572af7a95a7ac7ce",
+    "url": "/sportin/static/css/main.f461b73f.chunk.css"
+  },
+  {
+    "revision": "cb170ed7f435afc85587",
+    "url": "/sportin/static/js/2.27505fc1.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/sportin/static/js/2.27505fc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f25572af7a95a7ac7ce",
+    "url": "/sportin/static/js/main.dd6a3de8.chunk.js"
+  },
+  {
+    "revision": "99d1420970a3553b32df",
+    "url": "/sportin/static/js/runtime-main.511cd7f9.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/sportin/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/sportin/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/sportin/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/sportin/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/sportin/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/sportin/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/sportin/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/sportin/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/sportin/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/sportin/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "7b252ddc9fe964f498144ad01ae204b8",
+    "url": "/sportin/static/media/mockup_1.7b252ddc.png"
+  },
+  {
+    "revision": "ec519054405a2e03f862e27882d52553",
+    "url": "/sportin/static/media/mockup_2.ec519054.png"
+  }
+]);
